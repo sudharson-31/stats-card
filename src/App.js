200df90a459d17bf51fc044edf01ab5ec4314095
sudharson-31 <96968997+sudharson-31@users.noreply.github.com>
2022-06-07 17:33:33 +0530

    Development done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./main.css";
+import {isMobile} from "react-device-detect";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-card">
+      <div className="content-box">
+          <h1>
+          Get <span>insights</span> that help your business grow.
+          </h1>
+          <p>
+          Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency.
+          </p>
+          <div className="stats">
+            <span>10k+<p>companies</p></span>
+            <span>314 <p>templates</p></span>
+            <span>12m+<p>queries</p> </span>
+          </div>
+      </div>
+      <div className="banner-box">
+        <img src={process.env.PUBLIC_URL + isMobile?'/images/image-header-mobile.jpg':'/images/image-header-desktop.jpg'}></img>
+      </div>
+
     </div>
   );
 }
